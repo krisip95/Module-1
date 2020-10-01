cb18f28b7d852efb69f825dc3b8008011849ff02
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-10-01 17:29:53 +0300

    Add files via upload

---
+++ ColorRange.java
@@ -0,0 +1,49 @@
+package edu.smg;
+
+import java.util.Scanner;
+
+public class ColorRange {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		String color="";
+
+		double wavelength;
+
+		System.out.print("enter a wavelenght in nm (380-750) ? "); // first prompt
+
+		wavelength = input.nextDouble(); // then read keyboard
+
+		if ((wavelength >= 380) && (wavelength <= 750))
+
+			if (wavelength < 450)
+
+				color = "violet";
+
+			else if (wavelength < 495)
+
+				color = "blue";
+
+			else if (wavelength < 570)
+
+				color = "green";
+
+			else if (wavelength < 590)
+
+				color = "yellow";
+
+			else if (wavelength < 620)
+
+				color = "orange";
+
+			else if (wavelength <= 750)
+
+				color = "red";
+
+		System.out.println( wavelength + " is a " + color);
+
+	}
+
+}
