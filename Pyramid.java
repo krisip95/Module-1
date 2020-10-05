849bb4470f5e8d6a6320d9eef4ced3a59c7a19d9
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-10-05 15:38:54 +0300

    Add files via upload

--- Pyramid.java
+++ Pyramid.java
@@ -13,19 +13,22 @@ public class Pyramid {
   int n = input.nextInt();
    
   //Loop through the lines from 1 to n
-  for (int i = 1; i <= n; i++) {
+  for (int i = 0; i < n; i++) {
    // printing spaces, 2 at a time from j=1 to j= n-i 
-   for (int j = 1; j <= (n - i); j++) {
-    System.out.print("  ");
-   }
+//   for (int j = 1; j <= (n - i); j++) {
+//    System.out.print("  ");
+//   }
     
-   for (int j = i; j >= 1; j--) {
+   for (int j = n-i; j > 0; j--) {
     System.out.print(j + " ");
    }
+   for (int j = 2; j <=  n-i; j++) {
+	    System.out.print(j + " ");
+	   }
     
-   for (int j = 2; j <= i; j++) {
-    System.out.print(j + " ");
-   }
+//   for (int j = 2; j <= i; j++) {
+//    System.out.print(j + " ");
+//   }
    System.out.println();
  
   }
