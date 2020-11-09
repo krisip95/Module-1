2201ea1f9fce689487de231a2e26be056960a165
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-11-09 14:04:44 +0200

    Add files via upload

---
+++ StudentFinancialAid.java
@@ -0,0 +1,38 @@
+package edu.smg;
+
+import java.util.Scanner;
+
+public class StudentFinancialAid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student_11[] students = new Student_11[5]; //suzdavame masiv ot u4enitsi
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < 5; i++) {
+			String name = sc.nextLine();
+			double grade = sc.nextDouble();
+			sc.nextLine();
+			//students[i].name = name; greshno
+			//students[i].grade = grade; greshno
+			students[i] = new Student_11( name, grade); //suzdavame obekt u4enik
+		}
+		
+		displayStudentsEligible(students); //izvejdat se u4enitsite koito otgovarqt na iziskvaniqta
+
+	}
+	
+	public static void displayStudentsEligible(Student_11 students[]) {
+		for(Student_11 s : students) {
+			if( isStudentEligible(s)) {
+				System.out.println(s.name);
+				
+			}
+		}
+	}
+
+	private static boolean isStudentEligible(Student_11 student) {
+		return student.grade > 5.50;
+	}
+
+}
---
+++ Student_11.java
@@ -0,0 +1,16 @@
+package edu.smg;
+
+public class Student_11 {
+	public String name;
+	public double grade;
+	
+	public Student_11() { //prazen konstruktor
+		
+	}
+	
+	public Student_11( String name, double grade) {
+		this.name = name; //obrushtame se kum konkretniq obekt s this
+		this.grade = grade;
+	}
+
+}
