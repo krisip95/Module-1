a0ed3e912c8b7330cad099263ab11d08349c9f45
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-12-03 16:12:57 +0200

    Update Student_11.java

--- Student_11.java
+++ Student_11.java
@@ -4,12 +4,12 @@ public class Student_11 {
 	public String name;
 	public double grade;
 	
-	public Student_11() { //prazen konstruktor
+	public Student_11() { 
 		
 	}
 	
 	public Student_11( String name, double grade) {
-		this.name = name; //obrushtame se kum konkretniq obekt s this
+		this.name = name; 
 		this.grade = grade;
 	}
 
