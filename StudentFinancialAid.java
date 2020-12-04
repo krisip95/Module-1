d4385f5369940753444676c6eadb20b44d3f1d6c
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-12-04 14:10:02 +0200

    Update StudentFinancialAid.java

--- StudentFinancialAid.java
+++ StudentFinancialAid.java
@@ -6,20 +6,17 @@ public class StudentFinancialAid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student_11[] students = new Student_11[5]; //suzdavame masiv ot u4enitsi
+		Student_11[] students = new Student_11[5]; 
 		Scanner sc = new Scanner(System.in);
 		
 		for(int i = 0; i < 5; i++) {
 			String name = sc.nextLine();
 			double grade = sc.nextDouble();
 			sc.nextLine();
-			//students[i].name = name; greshno
-			//students[i].grade = grade; greshno
-			students[i] = new Student_11( name, grade); //suzdavame obekt u4enik
+			students[i] = new Student_11( name, grade); 
 		}
 		
-		displayStudentsEligible(students); //izvejdat se u4enitsite koito otgovarqt na iziskvaniqta
-
+		displayStudentsEligible(students); 
 	}
 	
 	public static void displayStudentsEligible(Student_11 students[]) {
