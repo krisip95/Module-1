67592de03428696b1f8bb4398dd6d861339755f2
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-12-07 12:28:55 +0200

    Update Account_14.java

--- Account_14.java
+++ Account_14.java
@@ -1,5 +1,63 @@
 package edu.smg;
 
-public class Account_14 {
+import java.util.Date;
 
+public class Account_14 {
+	private int id = 0;
+	private double balance;
+	private static double yearInterestRate;
+	private Date dateCreated = new Date();
+	
+	Account_14(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getYearInterestRate() {
+		return yearInterestRate;
+	}
+
+	public void setYearInterestRate(double yearInterestRate) {
+		Account_14.yearInterestRate = yearInterestRate;
+	}
+	public Date getDateCreated() { 
+		return dateCreated;
+	}
+	
+	public static double getMonthlyInterestRate() {
+		return yearInterestRate / 12;
+	}
+	public double getMonthlyInterest() {
+		return balance * getMonthlyInterestRate() / 100;
+	}
+	public void withdraw(double sumToWithdraw){
+		if(sumToWithdraw < 0 || balance < sumToWithdraw){
+			System.out.println("Error");
+			return;
+		}
+		balance -= sumToWithdraw; 
+	}
+	public void deposit(double sumToDeposit){
+		if(sumToDeposit < 0){
+			System.out.println("Error");
+			return;
+		}
+		balance += sumToDeposit;
+	}
 }
+	
