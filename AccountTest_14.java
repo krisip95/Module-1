4ee8e2449da3360af578c392d87499e031fb55aa
krisip95 <71447845+krisip95@users.noreply.github.com>
2020-12-07 14:43:37 +0200

    Update AccountTest_14.java

--- AccountTest_14.java
+++ AccountTest_14.java
@@ -5,6 +5,15 @@ public class AccountTest_14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Account_14 acc = new Account_14();
+		acc.setId(1122);
+		acc.setBalance(20000);
+		acc.setYearInterestRate(4.5);
+		acc.withdraw(2500);
+		acc.deposit(3000);
+		
+		System.out.println("Balance: " + acc.getBalance() + "\nMonthly inerest: " + acc.getMonthlyInterest() + 
+				"\nDate: " + acc.getDateCreated());
+		}
+	
 	}
-
-}
