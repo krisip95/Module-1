aece878ff6f6531ba48804c91ae36daa823045e3
krisip95 <71447845+krisip95@users.noreply.github.com>
2021-01-10 22:56:24 +0200

    Add files via upload

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,23 +1,5 @@
-package edu.smg;
+package edu.smg03;
 
-public class SavingsAccount {
-	public double balance; 
-	public double interestRate = 0.1;
-	public String name;
-	
-	public void deposit( double sum) {
-		balance += sum;
-		System.out.println("Successful deposit " + sum + " new balance " + balance);
-		
-	}
-	
-	public void withdraw( double sum) { 
-		if(sum < 0 || sum > balance) {
-			System.out.println("error");
-			return; 
-		}
-		balance -= sum;
-		System.out.println("Successful withdraw " + sum + " new balane " + balance);
-	}
+public class SavingsAccount extends Account {
 
 }
